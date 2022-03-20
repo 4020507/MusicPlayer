ec6a7a6eb7121640310fca7d703662e590659605
4020507 <4020507@naver.com>
2022-03-20 10:51:53 +0900

    make splash screen

---
+++ app/src/main/java/com/example/musicplayer/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.musicplayer
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+
+class SplashActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Glide.with(this)
+            .load(R.drawable.splash_image)
+            .into(findViewById(R.id.splash))
+
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },2000)
+    }
+}
